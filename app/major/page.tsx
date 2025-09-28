0acc8eed036df6045cefb683174c38711271fdad
Ntanatorn <torkaitai0404@gmail.com>
2025-09-28 17:48:23 +0700

    แก้ไข font ในเลเอ้า

--- app/major/page.tsx
+++ app/major/page.tsx
@@ -1,16 +1,25 @@
 import Menubar from "@/components/menubar";
 import Menumajor from "@/components/menumajor";
+import { Itim,Bungee } from 'next/font/google';
+const bungee = Bungee({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+const itim = Itim({
+  subsets: ["thai"],
+  weight: ["400"],
+});
 export default function Page() {
   return (
     <>
       <Menubar />
-      <h1 className="text-center pt-2.5">สาขาวิชาต่างๆ ในมหาวิทยาลัย</h1>
+      <h1 className={`${itim.className} text-center pt-2.5`}>สาขาวิชาต่างๆ ในมหาวิทยาลัย</h1>
       {/* ----------------ลิงค์แสดงเมนูไปยังวิชาสาขาวิชาต่างๆ-------------------- */}
 
       <Menumajor />
 
       {/* ---------------------------------------------------------------- */}
-      <hr /><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut sequi repellat et ut minima optio aspernatur ducimus veniam quos dignissimos.</p>
+      <hr /><p className= {`${bungee.className}`}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut sequi repellat et ut minima optio aspernatur ducimus veniam quos dignissimos.</p>
     </>
   );
 }
